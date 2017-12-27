8c3a201f12b89953ed3daa0043acf9ce7cbba545
liao <liaojava@163.com>
2017-12-27 10:27:49 +0800

    打印吴字

--- Demo/src/demo/demo.java
+++ Demo/src/demo/demo.java
@@ -3,8 +3,19 @@ package demo;
 public class demo {
 
 	public static void main(String[] args) {
-		System.out.println("first commit!!");
-		System.out.println("hello world!");
+		System.out.println("  **********");
+		System.out.println("  **      **");
+		System.out.println("  **      **");
+		System.out.println("  **********");
+		System.out.println("              ");
+		System.out.println("   ******** ");
+		System.out.println("      **      ");
+		System.out.println(" ************ ");
+		System.out.println("      **      ");
+		System.out.println("     *  *     ");
+		System.out.println("    **  **   ");
+		System.out.println("   **    **  ");
+		System.out.println("Œ‚");
 	}
 	
 }
